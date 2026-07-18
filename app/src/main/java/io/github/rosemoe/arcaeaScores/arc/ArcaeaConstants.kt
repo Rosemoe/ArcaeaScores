bb560fe3c2496dda1728aecdc8f33260b8de9ca3
Rosemoe <2073412493@qq.com>
2026-07-18 13:02:04 +0800

    chore: update constants and songlist to Arcaea v6.15.0

--- app/src/main/java/io/github/rosemoe/arcaeaScores/arc/ArcaeaConstants.kt
+++ app/src/main/java/io/github/rosemoe/arcaeaScores/arc/ArcaeaConstants.kt
@@ -6,15 +6,15 @@ import java.io.InputStream
 class ArcaeaConstants(constantsFile: InputStream) {
 
     private val mapping = mutableMapOf<String, DoubleArray>()
+    private val difficultyKeys = arrayOf("pst", "prs", "ftr", "byd", "etr")
 
     init {
         val json = JSONObject(constantsFile.reader().readText())
         for (key in json.keys()) {
-            val data = json.getJSONArray(key)
+            val data = json.getJSONObject(key)
             val const = DoubleArray(5) { 0.0 }
             for (i in 0 until 5) {
-                if (i < data.length())
-                    const[i] = (data.get(i) as? JSONObject)?.getDouble("constant") ?: 0.0
+                const[i] = data.optDouble(difficultyKeys[i], 0.0)
             }
             mapping[key] = const
         }
