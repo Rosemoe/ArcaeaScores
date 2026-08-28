e55f7e8307d56f529930b3194a94db56b4ec653f
Rosemoe <2073412493@qq.com>
2026-08-28 23:15:58 +0800

    chore: bump version to 2.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,8 +51,8 @@ android {
         applicationId = "io.github.rosemoe.arcaeaScores"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 22001
-        versionName = "2.2.1"
+        versionCode = 23000
+        versionName = "2.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
